2cd800c7e95a8c941502410f0d544de4f2a46cdb
Benjamin <gracielvelazquez464@gmail.com>
2025-02-18 19:53:22 -0700

    benjamin

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // config.js
 export const TMDB_API_KEY = "2cb1d37b6731f33714453a772c38e359";
-export const YOUTUBE_API_KEY = "AIzaSyCw0Dtr5HkjMxwWU_MqNmpAEK0tDUw-n2Y";
+export const YOUTUBE_API_KEY = "AIzaSyD70Pv65ADYuh9-Ne7SPbAlBQyz3eGKCBA";
